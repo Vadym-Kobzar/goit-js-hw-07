3829007810ae853e8c2b42616c5c6682a06a24b7
Vadym Kobzar <votvad@gmail.com>
2022-07-26 10:24:25 +0300

    nottheand

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,2 +1,30 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const itemsOfGallery = [];
+
+galleryItems.forEach((element) => {
+  /* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */
+  const galleryItem = document.createElement("div");
+  galleryItem.className = "gallery__item";
+
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = element.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute("data-source", element.original);
+  galleryImage.alt = element.description;
+
+  
